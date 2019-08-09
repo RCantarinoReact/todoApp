d87b83a6d908e0a3db206380d4a1fd63f7607aa4
Renato Cantarino <renato.cantarino@gmail.com>
2019-08-08 22:34:29 -0300

    evolucao

---
+++ backend/src/config/cors.js
@@ -0,0 +1,8 @@
+module.exports = function (req, res, next) {
+
+    res.header('Acess-Control-Allow-Origin' , '*')
+    res.header('Acess-Control-Allow-Methods' , 'GET,POST,OPTIONS,PUT,DELETE,PATCH')
+    res.header('Acess-Control-Allow-Headers' , 'Origin, X-Requested-With,Content-Type,Accept')
+    next()
+
+}
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,11 +3,13 @@ const porta = 3003
 const bodyParse = require('body-parser')
 const express = require('express')
 const server = express()
+const Cors = require('./cors')
 
 //mids - implementação padrao intercept filter pattern do chain of responsibility
 // é uma pipeline de operações
 server.use(bodyParse.urlencoded({ extended: true }))
 server.use(bodyParse.json())
+server.use(Cors)
 
 server.listen(porta, function () {
     console.log(`Backend rodando na porta ${porta}`)
